8d80176f8a7aed77dea8fbbe7c0af89da6594001
jonestrada7 <jonathan.estrada747@gmail.com>
2019-10-12 20:14:47 -0700

    Updated Projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./Projects.css"
 import githubImg from "../images/github.svg"
-import LinkbotImg1 from "../images/LinkbotImg1.jpg"
 import LinkbotImg2 from "../images/LinkbotImg2.jpg"
 import NodeSportsImg from "../images/node_sports.jpg"
 import BillSplitLogo from "../images/BillSplitLogo.png"
 import BillSplitExample from "../images/BillSplitExample.jpg"
+import DigitImg from "../images/DigitImg.png"
+import NscImg from "../images/nsc_img.JPG"
 import "aos/dist/aos.css"
 
 class Projects extends React.Component {
@@ -81,6 +82,48 @@ class Projects extends React.Component {
                     </div>
                     
                 </a>
+            </div>
+            
+            
+            <div className='content'>
+                <a target='_blank' href='https://github.com/jonestrada7/Digit_Recognition'
+                    rel='noopener noreferrer' data-aos='fade-right' data-aos-duration='750' className='proj-content'>
+                        
+                    <div className='project-base'>
+                        <div className='image-box'>
+                            <h2>Digit Classification</h2>
+                            <p className='proj-description'>Used <b>Tensorflow</b> and <b>Keras</b> to construct a <b>Convolutional Neural Network</b> trained on MNIST data to recognize, classify and distinguish the ten digits. Used <b>Pandas</b> to preprocess and prepare images from CSV files. Reached <b>98% accuracy</b> on validation set.</p>
+                            <img src={DigitImg} alt="" className='proj-img digitimg'></img>
+                        </div> 
+                    </div>
+                    
+                    <div className='overlay'>
+                        <div className='github-overlay'>
+                            <p className='github-desc'>Click to see the code!</p> 
+                            <img src={githubImg} alt='Octocat' className='gitimg'></img>
+                        </div>
+                    </div>
+                </a>
+                
+                <a target='_blank' href='https://github.com/jonestrada7/Natural-Scenery-Classification'
+                    rel='noopener noreferrer' data-aos='fade-left' data-aos-duration='750' className='proj-content'>
+                        
+                    <div className='project-base'>
+                        <div className='image-box'>
+                            <h2 id='nsc-h'>Natural Scenery Classification</h2>
+                            <p className='proj-description'><b>Convolutional Neural Network</b> trained on Natural Scenery data provided by Intel to recognize, classify and distinguish buildings, forests, glaciers, mountains, the sea, and streets. Used <b>OpenCV</b> and <b>NumPy</b> to preprocess and prepare .jpg images. Reached <b>83%</b> accuracy on validation set.</p>
+                            <img src={NscImg} alt="" className='proj-img nsc-img'></img>
+                        </div> 
+                    </div>
+                    
+                    <div className='overlay'>
+                        <div className='github-overlay'>
+                            <p className='github-desc'>Click to see the code!</p> 
+                            <img src={githubImg} alt='Octocat' className='gitimg'></img>
+                        </div>
+                    </div>
+                </a>
+                
             </div>
         </div>
     }
